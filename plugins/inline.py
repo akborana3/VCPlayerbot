74438f7f9c82fdfe8c629f86cd20ee9044c94c28
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-11-24 09:04:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,7 +31,7 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/akborana3/VCPlayerBot'),
         InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
     ]
     ]
@@ -47,7 +47,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/akborana3/VCPlayerBot) below.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
